3abde7fa3a85799c3addf3c1e459e3715c09a91d
Maycon Carlete <maycon.carlete@gmail.com>
2024-04-24 18:21:37 -0300

    support to fetchcontent

--- 17-sdl-project/Main.cpp
+++ 17-sdl-project/Main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     SDL_Window* window;
     SDL_Renderer* renderer;
 
- if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+ if(SDL_Init(SDL_INIT_VIDEO) != 0){
         fprintf(stderr, "Error initializing SDL.\n");
         return false;
     }
