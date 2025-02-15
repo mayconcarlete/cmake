97c453f428b8fb41eb2ba6a30137cb0188afbe43
Maycon Carlete <maycon.carlete@gmail.com>
2025-02-15 16:37:21 -0500

    wip

--- 18-http-requests/main.cpp
+++ 18-http-requests/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <curl/curl.h>
 
+
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
@@ -14,7 +15,7 @@ int main(){
 
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000");
+        curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.15.9:3001");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         res = curl_easy_perform(curl);
---
+++ 20-test-pre-compiled/main.cpp
@@ -0,0 +1,12 @@
+#include "calculator.h"
+#include <iostream>
+
+
+int main(){
+
+    const int result = sum(100, 200);
+
+    std::cout << "Valor result: " << result << std::endl;
+
+    return EXIT_SUCCESS;
+}
