eee85246beaf2400dfb57368da719d4c5d5527a6
Maycon Carlete <maycon.carlete@gmail.com>
2025-07-20 10:39:59 -0300

    add format

--- 30-threads-project/main.cpp
+++ 30-threads-project/main.cpp
@@ -2,27 +2,54 @@
 #include <queue>
 #include <thread>
 #include <boost/thread.hpp>
+#include <boost/lockfree/queue.hpp>
 #include <chrono>
+#include <string>
+#include <memory>
+#include <atomic>
+#include <fmt/core.h>
 
+struct Message {
+    int event_type;
+    int x;
+    int y;
+};
 
-void worker1(){
-    for(int i = 0; i < 5; ++i){
-        std::cout << "[Worker 1]: " << i << "\n";
-        std::this_thread::sleep_for(std::chrono::milliseconds(600));
+struct SharedData {
+    boost::lockfree::queue<Message> queue;
+    std::atomic<bool> done;
+    SharedData(): queue(128), done(false){}
+};
+
+void producer(std::shared_ptr<SharedData> shared_data){
+    for(int i = 0; i < 10; ++i){
+        auto event_name = fmt::format("Move: {}", i);
+        auto msg = Message{.event_type=i, .x=10, .y=50};
+        shared_data->queue.push(msg);
+        std::cout << "[Producer] Sent a message: " << i << "\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    shared_data->done = true;
 }
 
-void worker2(){
-    for(int i = 0; i < 5; ++i){
-        std::cout << "[Worker 2]: " << i << "\n";
-        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+void consumer(std::shared_ptr<SharedData> shared_data){
+    Message msg;
+    while(!shared_data->done || !shared_data->queue.empty()){
+        if(shared_data->queue.pop(msg)){
+            auto consumer_msg = fmt::format("[Consumer]: {} X: {} Y: {}", msg.event_type, msg.x, msg.y);
+            std::cout << consumer_msg << "\n";
+        } else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
     }
 }
 
 int main(){
 
-    boost::thread thread_worker1(worker1);
-    boost::thread thread_worker2(worker2);
+    auto shared_data = std::make_shared<SharedData>();
+
+    boost::thread thread_worker1(producer, shared_data);
+    boost::thread thread_worker2(consumer, shared_data);
 
     thread_worker1.join();
     thread_worker2.join();
